646982df8e7661e226b4dac2f84da2a692e1fd7d
Mounika Porandla <mouni.porandla@gmail.com>
2016-10-09 05:08:18 -0700

    changed

--- src/swarmBots/ROVER_01.java
+++ src/swarmBots/ROVER_01.java
@@ -226,7 +226,8 @@ public class ROVER_01{
 						// check scanMap to see if path is blocked to the south
 						// (scanMap may be old data by now)
 						if (scanMapTiles[centerIndex][centerIndex +1].getHasRover() 
-								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.ROCK
+								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SOIL
+								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.GRAVEL
 								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.SAND
 								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.NONE) {
 							blocked = true;
@@ -244,7 +245,8 @@ public class ROVER_01{
 						//System.out.println("ROVER_01 scanMapTiles[2][1].getTerrain() " + scanMapTiles[2][1].getTerrain().toString());
 						
 						if (scanMapTiles[centerIndex][centerIndex -1].getHasRover() 
-								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.ROCK
+								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SOIL
+								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.GRAVEL
 								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SAND
 								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.NONE) {
 							blocked = true;
